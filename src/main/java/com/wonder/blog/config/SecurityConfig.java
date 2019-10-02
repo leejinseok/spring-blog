9ebef1d6e84f0d436914f7a5b63e7f60e2474a58
leejinseok <sonaky47@naver.com>
2019-10-02 17:21:46 +0900

    update

--- src/main/java/com/wonder/blog/config/SecurityConfig.java
+++ src/main/java/com/wonder/blog/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.wonder.blog.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wonder.blog.security.AjaxAuthenticationProvider;
 import com.wonder.blog.security.AjaxLoginProcessingFilter;
+import com.wonder.blog.security.AuthFailureHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.WebSecurityEnablerConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,13 +18,52 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalAuthentication
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+  public static final String AUTHENTICATION_URL = "/api/auth/login";
+
+  @Autowired private AuthenticationSuccessHandler successHandler;
+  @Autowired private AuthenticationFailureHandler failureHandler;
+  @Autowired private AjaxAuthenticationProvider ajaxAuthenticationProvider;
+  @Autowired private AuthenticationManager authenticationManager;
+
+  @Autowired private ObjectMapper objectMapper;
+
+  protected AjaxLoginProcessingFilter buildAjaxLoginProcessingFilter(String loginEntryPoint) throws Exception {
+    AjaxLoginProcessingFilter filter = new AjaxLoginProcessingFilter(loginEntryPoint, successHandler, failureHandler, objectMapper);
+    filter.setAuthenticationManager(this.authenticationManager);
+    return filter;
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) {
+    auth.authenticationProvider(ajaxAuthenticationProvider);
+  }
+
   @Bean
   public BCryptPasswordEncoder bCryptPasswordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+      .csrf().disable()
+      .exceptionHandling()
+      .and()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+      .and()
+          .addFilterBefore(buildAjaxLoginProcessingFilter(AUTHENTICATION_URL), UsernamePasswordAuthenticationFilter.class);
   }
 }
--- src/main/java/com/wonder/blog/security/AuthFailureHandler.java
+++ src/main/java/com/wonder/blog/security/AuthFailureHandler.java
@@ -3,11 +3,13 @@ package com.wonder.blog.security;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class AuthFailureHandler extends SimpleUrlAuthenticationFailureHandler {
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
                                         AuthenticationException exception) throws IOException {
