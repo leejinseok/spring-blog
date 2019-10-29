0e3406a6dd8b10ecfd269c158ab4c5970ea26aca
이진석 <sonaky47@gmail.com>
2019-10-29 18:40:01 +0900

    code refactoring

--- backend/src/main/java/com/wonder/blog/repository/UserRepository.java
+++ backend/src/main/java/com/wonder/blog/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.wonder.blog.repository;
 
-
 import com.wonder.blog.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
-
 
 public interface UserRepository extends CrudRepository<User, Integer> {
   Optional<User> findByEmail(String email);
