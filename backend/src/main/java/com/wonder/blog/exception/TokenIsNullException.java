30b4f992233a4bb474865024e8c8262f7b52e53c
이진석 <sonaky47@gmail.com>
2019-10-29 18:36:01 +0900

    CustomException refactoring

--- backend/src/main/java/com/wonder/blog/exception/TokenIsNullException.java
+++
@@ -1,9 +0,0 @@
-package com.wonder.blog.exception;
-
-import org.springframework.security.core.AuthenticationException;
-
-public class TokenIsNullException extends AuthenticationException {
-  public TokenIsNullException(String msg) {
-    super(msg);
-  }
-}
