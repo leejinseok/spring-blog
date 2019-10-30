9f7c2bd2eb82af550adcafddc3d8762d3dbf5d8d
이진석 <sonaky47@gmail.com>
2019-10-30 19:01:38 +0900

    work little

--- backend/src/main/java/com/wonder/blog/config/AwsProperties.java
+++ backend/src/main/java/com/wonder/blog/config/AwsProperties.java
@@ -2,15 +2,9 @@ package com.wonder.blog.config;
 
 import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Component
