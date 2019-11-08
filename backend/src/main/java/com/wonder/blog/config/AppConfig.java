b648fade57d4ff98164f7b7d1d9f4770aeb59b2f
이진석 <sonaky47@gmail.com>
2019-11-08 12:00:32 +0900

    default data insert on boot running

--- backend/src/main/java/com/wonder/blog/config/AppConfig.java
+++ backend/src/main/java/com/wonder/blog/config/AppConfig.java
@@ -1,13 +1,18 @@
 package com.wonder.blog.config;
 
+import com.wonder.blog.entity.Post;
 import com.wonder.blog.entity.User;
-import com.wonder.blog.service.UserService;
+import com.wonder.blog.repository.PostRepository;
+import com.wonder.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.time.LocalDateTime;
 
 @Configuration
 public class AppConfig {
@@ -17,17 +22,33 @@ public class AppConfig {
     public ApplicationRunner applicationRunner() {
         return new ApplicationRunner() {
             @Autowired
-            UserService userService;
+            UserRepository userRepository;
+
+            @Autowired
+            PostRepository postRepository;
+
+            @Autowired
+            BCryptPasswordEncoder bCryptPasswordEncoder;
 
             @Override
             public void run(ApplicationArguments args) {
+
                 User user = User.builder()
                   .email("sonaky47@naver.com")
-                  .password("1111")
                   .name("leejinseok")
+                  .password(bCryptPasswordEncoder.encode("1111"))
                   .build();
 
-                userService.addUser(user);
+                userRepository.save(user);
+
+                Post post = Post.builder()
+                  .title("자자 ...")
+                  .user(user)
+                  .content("무엇을 쓸까 ...")
+                  .createdAt(LocalDateTime.now())
+                  .build();
+
+                postRepository.save(post);
             }
         };
     }
