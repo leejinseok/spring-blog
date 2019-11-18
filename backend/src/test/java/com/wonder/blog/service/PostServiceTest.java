9fc706d00907d706234c80524977188b2cac0c62
이진석 <sonaky47@gmail.com>
2019-11-18 16:48:03 +0900

    addPost test code added

--- backend/src/test/java/com/wonder/blog/service/PostServiceTest.java
+++ backend/src/test/java/com/wonder/blog/service/PostServiceTest.java
@@ -1,6 +1,8 @@
 package com.wonder.blog.service;
 
 import com.wonder.blog.common.TestDescription;
+import com.wonder.blog.domain.Post;
+import com.wonder.blog.dto.PostDto;
 import com.wonder.blog.exception.DataNotFoundException;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,6 +13,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -24,6 +27,18 @@ public class PostServiceTest {
 
   @Autowired
   PostService postService;
+
+  @Test
+  @Transactional
+  public void addPost() {
+    PostDto.RegisterReq dto = new PostDto.RegisterReq();
+    dto.setTitle("글을 써보자");
+    dto.setContent("내용을 ...");
+    dto.setPostTags("[]");
+    Post newPost = postService.addPost(dto);
+
+    assertThat(newPost).hasFieldOrProperty("id");
+  }
 
   @Test
   @TestDescription("존재하지 않는 게시글 조회하는 경우")
