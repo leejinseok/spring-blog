67e6d95818dfffe47c99cd56a4699d4e94c4212a
이진석 <sonaky47@gmail.com>
2019-11-18 12:49:18 +0900

    spring-boot-starter-data-redis 추가

--- backend/src/main/java/com/wonder/blog/BlogApplication.java
+++ backend/src/main/java/com/wonder/blog/BlogApplication.java
@@ -3,7 +3,9 @@ package com.wonder.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class BlogApplication {
   public static void main(String[] args) {
